f86a1d308179f080ac1136319db822822ef0f60a
duahuss <109004925+duahuss@users.noreply.github.com>
2023-09-30 14:11:52 -0400

    Changes to main

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,18 +1,27 @@
 import okhttp3.*;
 import java.io.IOException;
 
-
 public class Main {
 
     public static void main(String[] args) throws Exception {
-        OkHttpClient client = new OkHttpClient();
+        try{
+            OkHttpClient client = new OkHttpClient();
 
-        Request request = new Request.Builder()
-                .url("https://api.edamam.com/api/recipes/v2?app_id=0e94da52&app_key=%20a1c655a3813bf3c3fc6362ee953aa8e3&type=public&mealType=Dinner")
-                .get()
-                .build();
+            Request request = new Request.Builder()
+                    .url("https://api.edamam.com/api/recipes/v2?app_id=0e94da52&app_key=%20a1c655a3813bf3c3fc6362ee953aa8e3&type=public&mealType=Dinner")
+                    .get()
+                    .build();
 
-        Response response = client.newCall(request).execute();
-        System.out.println(response);
+            Response response = client.newCall(request).execute();
+            if (response.isSuccessful()) {
+                assert response.body() != null;
+                System.out.println(response.body().string());
+            }
+            response.close();
+        } catch(IOException e) {
+            throw new IOException("error");
+
+        }
+
     }
 }
